1e77f8da666edf3c98cabb3adf1b26b86f8a2ffa
acarreiragonzalez <acarreiragonzalez@eniac25.danielcastelao.org>
2014-10-07 09:10:55 +0200

    Programacion boletin 2.2

---
+++ Boletin2.2/src/boletin2/pkg2/Boletin22.java
@@ -0,0 +1,19 @@
+
+package boletin2.pkg2;
+
+import java.util.Scanner;
+
+
+public class Boletin22 {
+
+   
+    public static void main(String[] args) {
+        float lado, area ;
+        System.out.println("lado");
+        Scanner dato= new Scanner (System.in);
+        lado = dato.nextFloat();
+        System.out.println("area="+ (lado*lado));
+ 
+    }
+    
+}
